ab10e69558d6e6d36dbbc40cddc17bb31815f172
Yadamzer <yadamzerterefe09@gmail.com>
2024-10-19 23:09:09 +0300

    Task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
 
-CREATE VIEW view_table AS SELECT origin, fans AS nb_fans FROM metal_bands;
-SELECT * FROM view_table;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
