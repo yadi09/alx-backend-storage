9430b3f182b77bca8ba3a645bb6b1a8708990727
Yadamzer <yadamzerterefe09@gmail.com>
2024-10-19 23:22:17 +0300

    Task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,6 @@
 -- as their main style, ranked by their longevity
 
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
+       FROM metal_bands
+       WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+       ORDER BY lifespan DESC;
